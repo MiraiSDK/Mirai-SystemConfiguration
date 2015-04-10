ef72dbc80717ad66cb6f4fbd00c1b1c355fe25a3
Yonghui Chen <chyhfj@gmail.com>
2015-04-10 14:15:53 +0800

    Initial Commit

---
+++ SystemConfiguration/SystemConfiguration.h
@@ -0,0 +1,19 @@
+//
+//  SystemConfiguration.h
+//  SystemConfiguration
+//
+//  Created by Chen Yonghui on 4/10/15.
+//  Copyright (c) 2015 Shanghai TinyNetwork Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SystemConfiguration.
+FOUNDATION_EXPORT double SystemConfigurationVersionNumber;
+
+//! Project version string for SystemConfiguration.
+FOUNDATION_EXPORT const unsigned char SystemConfigurationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SystemConfiguration/PublicHeader.h>
+
+
